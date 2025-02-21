49b055b0f01708fbf204d2751d8548191d40daa2
Rahul kumar <149350884+rahulkumar2112k@users.noreply.github.com>
2025-02-21 19:16:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -41,4 +41,5 @@ def handle_get():
     return jsonify({'operation_code': 1})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the server on 0.0.0.0 for external accessibility with port 5000
+    app.run(host='0.0.0.0', port=5000, debug=True)
